cc19d61b12bac59d5f5a298f4d6fb404c5129dde
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-06 16:31:30 -0300

    feat: post endpoint for sign-up and tweets and get endpoint for tweets

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
 import express from 'express'
 import cors from 'cors'
+import { t } from 'tar'
 
 const app = express()
 app.use(cors())
+app.use(express.json())
+
+const users = []
+const tweets = []
 
 app.get("/",(req,res)=>{
     console.log("deu get")
-    res.send("lalalla")
+    res.send("lallla")
+})
+
+app.get("/tweets",(req,res)=>{
+    while (tweets.length >10){
+        tweets.shift()
+    }
+    const tweetsAndAvatars = tweets.map(t=>{
+        return {...t, avatar:users.find(t.username).avatar}
+    })
+    res.send(tweetsAndAvatars)
+})
+
+app.post("/sign-up",(req,res)=>{
+    const data = req.body
+    users.push(data)
+    res.send("OK")
+})
+
+app.post("/tweets",(req,res)=>{
+    const usersername = req.body.username
+    if(users.find(u => u.username === username)){
+        tweets.push(req.body.tweet)
+        res.send("OK")
+    }
 })
 
 app.listen(5000, ()=>console.log("Server on"))

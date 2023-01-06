cc8ef12e9ee1e4415757f0c57ed566b2510f9e35
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-06 16:59:56 -0300

    fixed bugs in post tweet

--- src/app.js
+++ src/app.js
@@ -6,36 +6,44 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const users = []
-const tweets = []
+const users = [{
+    username: "bobesponja",
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+}]
+const tweets = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub"
+},
+{
+    username: "bobesponja",
+    tweet: "eu odeio o hub"
+}
+]
 
-app.get("/",(req,res)=>{
-    console.log("deu get")
-    res.send("lallla")
-})
 
-app.get("/tweets",(req,res)=>{
-    while (tweets.length >10){
+app.get("/tweets", (req, res) => {
+    while (tweets.length > 10) {
         tweets.shift()
     }
-    const tweetsAndAvatars = tweets.map(t=>{
-        return {...t, avatar:users.find(t.username).avatar}
+    const tweetsAndAvatars = tweets.map(t => {
+        const avatar = users.find(u=>t.username===u.username).avatar
+        return { ...t, avatar }
     })
     res.send(tweetsAndAvatars)
 })
 
-app.post("/sign-up",(req,res)=>{
+app.post("/sign-up", (req, res) => {
     const data = req.body
     users.push(data)
     res.send("OK")
 })
 
-app.post("/tweets",(req,res)=>{
-    const usersername = req.body.username
-    if(users.find(u => u.username === username)){
-        tweets.push(req.body.tweet)
+app.post("/tweets", (req, res) => {
+    const username = req.body.username
+    if (users.find(u => u.username === username)) {
+        tweets.push(req.body)
         res.send("OK")
     }
 })
 
-app.listen(5000, ()=>console.log("Server on"))
+app.listen(5000, () => console.log("Server on"))
